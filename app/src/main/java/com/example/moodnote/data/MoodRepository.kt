6d62ec68d98377c8a13f4e4e941ef469cffb0b26
Ali <lozovaalina05122001@gmail.com>
2025-09-12 17:11:10 +0300

    Реализованы MoodRepository и MoodNote

---
+++ app/src/main/java/com/example/moodnote/data/MoodRepository.kt
@@ -0,0 +1,21 @@
+package com.example.moodnote.data
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class MoodRepository @Inject constructor() {
+    private val _notes = MutableStateFlow<List<MoodNote>>(emptyList())
+    val notes: StateFlow<List<MoodNote>> = _notes.asStateFlow()
+
+    fun addNote(note: MoodNote) {
+        _notes.value = _notes.value + note
+    }
+
+    fun deleteNote(id: Int) {
+        _notes.value = _notes.value.filter { it.id != id }
+    }
+}
