2da2511112a187c8f2fb2263e7cb9cbb9e10dc4c
Ali <lozovaalina05122001@gmail.com>
2025-09-23 10:58:38 +0300

    Создали MoodListScreen

--- app/src/main/java/com/example/moodnote/screens/MoodListScreen.kt
+++ app/src/main/java/com/example/moodnote/screens/MoodListScreen.kt
@@ -1,12 +1,26 @@
 package com.example.moodnote.screens
 
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.moodnote.viewModel.MoodViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
-fun MoodListScreen() {
+fun MoodListScreen(
+    viewModel: MoodViewModel,
+    onAddClick: () -> Unit
+) {
+    val notes = viewModel.notes.collectAsState().value
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(onClick = onAddClick) {
@@ -14,6 +28,50 @@ fun MoodListScreen() {
             }
         }
     ) { padding ->
-
+        if (notes.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .padding(padding),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Нет заметок. Добавь первую!"
+                )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .padding(16.dp)
+            ) {
+                items(notes) { note ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 4.dp)
+                            .clickable { viewModel.deleteNote(note.id) },
+                        elevation = CardDefaults.cardElevation(4.dp)
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "${note.emoji} ${note.description}",
+                                modifier = Modifier
+                                    .weight(1f)
+                            )
+                            Text(
+                                text = SimpleDateFormat("dd/mm/yyyy", Locale.getDefault())
+                                    .format(Date(note.timeStamp)),
+                                style = MaterialTheme.typography.bodySmall
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
